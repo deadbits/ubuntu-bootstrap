2e62b456fe717af67f097bb94283c0154f3e80b6
adam swanda <deadbits@users.noreply.github.com>
2016-07-03 21:01:38 -0400

    update conf file path

--- core/vim.sh
+++ core/vim.sh
@@ -1,7 +1,7 @@
 echo "[ Vim setup ]"
 echo "Copying dotfile to /usr/share/vim/vimrc ..."
-sudo cp $repo/conf/dotfiles/vim/vimrc /usr/share/vim/vimrc
+sudo cp $repo/conf/vim/vimrc /usr/share/vim/vimrc
 echo "Creating local directory $HOME/.vim/colors ..."
 mkdir -p $HOME/.vim/colors
 echo "Copying vim color schemes ..."
-cp $repo/conf/dotfiles/vim/colors/* $HOME/.vim/colors
+cp $repo/conf/vim/colors/* $HOME/.vim/colors
