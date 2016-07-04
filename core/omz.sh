1edcdf0ff33224ca3ecb18566fbbc7e4bf519cd2
adam swanda <deadbits@users.noreply.github.com>
2016-07-03 21:00:13 -0400

    change conf file path

--- core/omz.sh
+++ core/omz.sh
@@ -1,4 +1,4 @@
 echo "[ oh-my-zsh setup ]"
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 echo "Copying zshrc template to $HOME/.zshrc ..."
-cp $repo/conf/dotfiles/zsh/.zshrc $HOME/.zshrc
+cp $repo/conf/zsh/.zshrc $HOME/.zshrc
